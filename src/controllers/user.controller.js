5241a14819436f1e4b939df4e4929d8f1774d647
HederAlves <alvesheder92@gmail.com>
2022-07-02 23:46:09 -0300

    Upload
    Create project

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,19 @@
+import { CreateUserService } from "../services/create.user.service.js";
+
+class UserController {
+  handle(request, response) {
+    const { body } = request;
+    
+    try {
+      const createUserService = new CreateUserService();
+      const result = createUserService.execute(body);
+      return response.json(result);
+    } catch (err) {
+      return response.status(400).json({
+        err: err.message,
+      });
+    }
+  }
+}
+
+export { UserController };
